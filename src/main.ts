d0b58c0f8fc196b3689f34f5f22cde6aa005e799
unfiltered-dev <128378292+unfiltered-dev@users.noreply.github.com>
2024-02-27 16:28:52 +0700

    ADD - packages need for this project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Initial middleware
+  app.use(morgan('dev'));
+  app.use(helmet());
+  app.use(compression());
+
+  // Running app
+  await app.listen(3000, () => {
+    Logger.log('Server is running at port: 3000');
+  });
 }
 bootstrap();
